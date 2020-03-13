b26ec90329ad1a95ea59e45b5e527881cbdb99e6
Eden Fils <ednfils@gmail.com>
2020-03-12 22:46:21 -0400

    Basic Request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SimpleHttpComponent } from './simple-http/simple-http.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleHttpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/simple-http/simple-http.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-simple-http',
+  templateUrl: './simple-http.component.html',
+  styleUrls: ['./simple-http.component.css']
+})
+export class SimpleHttpComponent implements OnInit {
+
+  data: Object;
+  loading: boolean;
+
+  constructor(private http: HttpClient) { }
+
+
+  makeRequest(): void {
+    this.loading = true;
+
+    this.http
+      .get('https://jsonplaceholder.typicode.com/posts/1')
+      // tslint:disable-next-line: align
+      .subscribe(data => {
+        this.data = data;
+        this.loading = false;
+      });
+  }
+
+  ngOnInit() {
+  }
+
+}
